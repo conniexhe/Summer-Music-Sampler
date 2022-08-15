175bf068586daf5f48482ff621da2a5dff499859
conniexhe <83856884+conniexhe@users.noreply.github.com>
2022-08-14 22:48:35 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,289 @@
+const APIController = (function() {
+
+  const clientId = 'b53826bfdbcd4eaea2d6c8fd331cb930';
+  const clientSecret = '4488b2d166c346b7b3a46899f6ac40dd';
+
+    // private methods
+    const _getToken = async () => {
+
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Authorization' : 'Basic ' + btoa( clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        return data.access_token;
+    }
+
+    const _getPlaylist = async (token) => {
+
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories/summer/playlists?limit=50`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.playlists.items;
+    }
+
+    const _getTracks = async (token, tracksEndPoint) => {
+
+        const limit = 50;
+
+        const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.items;
+    }
+
+    const _getTrack = async (token, trackEndPoint) => {
+
+        const result = await fetch(`${trackEndPoint}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data;
+    }
+
+    return {
+        getToken() {
+            return _getToken();
+        },
+
+        getPlaylist(token) {
+            return _getPlaylist(token);
+        },
+        getTracks(token, tracksEndPoint) {
+            return _getTracks(token, tracksEndPoint);
+        },
+        getTrack(token, trackEndPoint) {
+            return _getTrack(token, trackEndPoint);
+        }
+    }
+})();
+
+
+// UI Module
+const UIController = (function() {
+
+    //object to hold references to html selectors
+    const DOMElements = {
+        selectPlaylist: '#select_playlist',
+        buttonSubmit: '#btn_submit',
+        divSongDetail: '#song-detail',
+        hfToken: '#hidden_token',
+        divSonglist: '.song-list'
+    }
+
+    //public methods
+    return {
+
+        //method to get input fields
+        inputField() {
+            return {
+                playlist: document.querySelector(DOMElements.selectPlaylist),
+                tracks: document.querySelector(DOMElements.divSonglist),
+                submit: document.querySelector(DOMElements.buttonSubmit),
+                songDetail: document.querySelector(DOMElements.divSongDetail)
+            }
+        },
+
+        createPlaylist(text, value) {
+            const html = `<option value="${value}">${text}</option>`;
+            document.querySelector(DOMElements.selectPlaylist).insertAdjacentHTML('beforeend', html);
+        },
+
+        // need method to create a track list group item
+        createTrack(id, name) {
+            const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`;
+            document.querySelector(DOMElements.divSonglist).insertAdjacentHTML('beforeend', html);
+        },
+
+        // need method to create the song detail
+        playTrack(id) {
+
+            const detailDiv = document.querySelector(DOMElements.divSongDetail);
+            // any time user clicks a new song, we need to clear out the song detail div
+            detailDiv.innerHTML = '';
+
+            const html =
+            `
+            <p><b>Now Playing: </b></p>
+            <iframe src="https://open.spotify.com/embed/track/${id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            `;
+
+            detailDiv.insertAdjacentHTML('beforeend', html);
+        },
+
+        resetTrackDetail() {
+            this.inputField().songDetail.innerHTML = '';
+        },
+
+        resetTracks() {
+            this.inputField().tracks.innerHTML = '';
+            this.resetTrackDetail();
+        },
+
+        resetPlaylist() {
+            this.inputField().playlist.innerHTML = '';
+            this.resetTracks();
+        }
+    }
+
+})();
+
+const APPController = (function(UICtrl, APICtrl) {
+
+    // get input field object ref
+    const DOMInputs = UICtrl.inputField();
+
+    // get playlists on page load
+    const loadPlaylists = async () => {
+        //get the token
+        const token = await APICtrl.getToken();
+        // get the playlist
+        const playlist = await APICtrl.getPlaylist(token);
+        // create a playlist list item for every playlist returned
+        playlist.forEach(p => UICtrl.createPlaylist(p.name, p.tracks.href));
+    }
+
+    // create playlist change event listener
+    DOMInputs.playlist.addEventListener('change', async () => {
+        error; // won't work without an error here ???
+        // clear playlist
+        UICtrl.resetPlaylist();
+        //get the token
+        const token = await APICtrl.getToken();
+        // get the playlist
+        const playlist = await APICtrl.getPlaylist(token);
+        // create a playlist list item for every playlist returned
+        playlist.forEach(p => UICtrl.createPlaylist(p.name, p.tracks.href));
+    });
+
+
+    // create submit button click event listener
+    DOMInputs.submit.addEventListener('click', async (e) => {
+        // prevent page reset
+        e.preventDefault();
+        // clear tracks
+        UICtrl.resetTracks();
+        //get the token
+        const token = await APICtrl.getToken();
+        // get the playlist field
+        const playlistSelect = UICtrl.inputField().playlist;
+        // get track endpoint based on the selected playlist
+        const tracksEndPoint = playlistSelect.options[playlistSelect.selectedIndex].value;
+        // get the list of tracks
+        const tracks = await APICtrl.getTracks(token, tracksEndPoint);
+        // create a track list item
+        tracks.forEach(el => UICtrl.createTrack(el.track.href, el.track.name))
+    });
+
+    // create song selection click event listener
+    DOMInputs.tracks.addEventListener('click', async (e) => {
+        // prevent page reset
+        e.preventDefault();
+        UICtrl.resetTrackDetail();
+        // get the token
+        const token = await APICtrl.getToken();
+        // get the track endpoint
+        const trackEndpoint = e.target.id;
+        //get the track object
+        const track = await APICtrl.getTrack(token, trackEndpoint);
+        // play track in browser
+        UICtrl.playTrack(track.id);
+    });
+
+    return {
+        init() {
+            console.log('App is starting');
+            loadPlaylists();
+        }
+    }
+
+})(UIController, APIController);
+
+// will need to call a method to load the playlists on page load
+APPController.init();
+
+document.addEventListener('DOMContentLoaded', function(){
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
+    script.onload = function(){
+        particlesJS("watermelons", {
+            "particles": {
+                "number": {
+                    "value": 15,
+                    "density": {
+                        "enable": true,
+                        "value_area": 1000
+                    }
+                },
+                "color": {
+                    "value": "#ffffff"
+                },
+                "shape": {
+                    "type": "image",
+                    "image": {
+                        "src": "watermelon.png",
+                        "width": 10,
+                        "height": 10
+                    }
+                },
+                "opacity": {
+                    "value": 0.7,
+                    "random": false,
+                    "anim": {
+                        "enable": false
+                    }
+                },
+                "size": {
+                    "value": 50,
+                    "random": true,
+                    "anim": {
+                        "enable": false
+                    }
+                },
+                "line_linked": {
+                    "enable": false
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 5,
+                    "direction": "bottom",
+                    "random": true,
+                    "straight": false,
+                    "out_mode": "out",
+                    "bounce": false,
+                    "attract": {
+                        "enable": true,
+                        "rotateX": 300,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": false
+                    },
+                    "onclick": {
+                        "enable": false
+                    },
+                    "resize": false
+                }
+            },
+            "retina_detect": true
+        });
+    }
+    document.head.append(script);
+});
